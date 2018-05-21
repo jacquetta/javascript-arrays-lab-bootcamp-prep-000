1af07cda87b96ad51323ef92c2be75045193ecea
Jacquetta Bommuluri <jacquettavb@hotmail.com>
2018-05-21 20:51:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const app = "I don't do much."
+// const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name, a){
+  return name.push(a);
+}
+destructivelyAppendKitten();
+
+function destructivelyPrependKitten(name, a){
+  return name.unshift(a);
+}
+destructivelyPrependKitten();
+
+function destructivelyRemoveLastKitten(name, a){
+  return name.pop(a);
+}
+destructivelyRemoveLastKitten();
+
+function destructivelyRemoveFirstKitten(name, a){
+  return name.shift(a);
+}
+destructivelyRemoveFirstKitten();
+
+function removeFirstKitten(a){
+  return a.slice(1);
+}
+removeFirstKitten();
